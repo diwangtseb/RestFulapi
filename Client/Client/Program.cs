ca3f292f93dd49c556ae95425d5223ecf624f6c3
wangdi <839639311@qq.com>
2020-02-10 13:41:43 +0800

    !

---
+++ Client/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ChannelFactory<IAddress> channelFactory = new ChannelFactory<IAddress>("addressService"))
+            {
+                IAddress iAddress = channelFactory.CreateChannel();
+                Address address = iAddress.Get("210.75.225.254");
+
+                if (address != null)
+                {
+                    Console.WriteLine(string.Format("IP来自{0}，{1}", address.Province, address.City));
+                }
+            }
+        }
+    }
+}
---
+++ Service/Service/IAddress.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Runtime.Serialization;
+
+namespace Service.Interface
+{
+    [ServiceContract]
+    public interface IAddress
+    {
+        [WebGet(UriTemplate = "ip={ip}",ResponseFormat =WebMessageFormat.Json)]
+        Address Get(string ip);
+    }
+
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public string IPAddress { get; set; }
+
+        [DataMember]
+        public string Province { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+    }
+}
