f1c28270ed71c7a49ce036c2b751f41584262d99
wangdi <839639311@qq.com>
2020-02-10 13:49:41 +0800

    重新添加项目

---
+++ RestFulApii/Service/Service/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebServiceHost host = new WebServiceHost(typeof(AddressService)))
+            {
+                host.Open();
+                Console.Read();
+            }
+        }
+    }
+}
